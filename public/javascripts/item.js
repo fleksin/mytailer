cca52a38ed48ea5b692b797fbbf50878af93c3b3
fleksin <hezhoufan@gmail.com>
2015-11-24 09:33:58 -0800

    添加下订单时的清单

--- public/javascripts/item.js
+++ public/javascripts/item.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-	
+	$('button').click(function(event){
+		var price;
+		if(!$("input[name='fabric']:checked").val()){
+			$(this).append("<input name='fabric' value='"+ $('#fabric').text() +"' style='display:none'>");
+			price=$('#price').text();
+		}
+		else{
+			var pricePlus=$("input[name='fabric']:checked").attr('price');
+			price= pricePlus + $('#price').text() ;
+			$(this).append("<input name='price' value='"+ price +"' style='display:none'>");
+			$(this).append("<input name='pricePlus' value='" + pricePlus + "' style='display:none'>");
+		}
+	})
 });
