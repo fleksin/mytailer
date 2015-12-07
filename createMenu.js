1005d18a7e97301202e4c0c434e1b4090af77260
Zhoufan <hezhoufan@gmail.com>
2015-12-07 20:43:50 +0000

    tried wechat-template, no luck yet

--- createMenu.js
+++ createMenu.js
@@ -3,28 +3,56 @@ var appid = 'wxaaa2eb5129f710a5';
 var appsecret = 'd15fb1721506a73cd77ca23e1060170a';
 var api = new wechatAPI(appid, appsecret);	
 	
-var menu = 
-	{
- 	"button":[
-   	   {
-	     "type":"view",
-	     "name":"Go to Demo",
-	     "url":"http://thirdtry.cloudapp.net/"
-	   },
-	   {
-	     "name":"opt",
-	     "sub_button":[
-	       {
-	         "type":"click",
-	         "name":"scarlet",
-	         "key":"V1001_GOOD"
-	       },
-	       {
-	         "type":"click",
-	         "name":"GG",
-	         "key":"V1001_GOOD"
-	       }]
-	   }]
-	};
+//var menu = 
+//	{
+// 	"button":[
+//   	   {
+//	     "type":"view",
+//	     "name":"Go to Demo",
+//	     "url":"http://thirdtry.cloudapp.net/"
+//	   },
+//	   {
+//	     "name":"opt",
+//	     "sub_button":[
+//	       {
+//	         "type":"click",
+//	         "name":"scarlet",
+//	         "key":"V1001_GOOD"
+//	       },
+//	       {
+//	         "type":"click",
+//	         "name":"GG",
+//	         "key":"V1001_GOOD"
+//	       }]
+//	   }]
+//	};
 
-api.createMenu(menu, function(err, res){if(err) console.log(err); console.log(res)});
+var templateId = '9lZRf9WlDz9mgcChIna8fZDBfbSBZPKVlRAy0jY0Meo';
+	var url = 'http://thirdtry.cloudapp.net/myorders';
+	var data = {
+	   "first": {
+	     "value":"Dear tailer, you have a new order placed",
+	     "color":"#173177"
+	   },
+	   "OrderSn":{
+	     "value":"111111",
+	     "color":"#173177"
+	   },
+	   "OrderStatus": {
+	     "value":"The buyer has placed an order",
+	     "color":"#173177"
+	   },
+	   "remark":{
+	     "value":"let's try",
+	     "color":"#173177"
+	   }
+	};
+	//var api = new wechatAPI(appid, appsecret);
+	api.sendTemplate('OPENID', templateId, url, data,   
+		function(err, result){
+			if(err) console.log(err);
+			else console.log(result);
+		}
+	);
+
+
--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,40 @@ var wechatAPI = require('wechat-api');
 var appid = 'wxaaa2eb5129f710a5';
 var appsecret = 'd15fb1721506a73cd77ca23e1060170a';
 var http = require('http');
+var api = new wechatAPI(appid, appsecret);
 
 /* GET users listing. */
-router.use('/', function(req, res, next) {
- 	wechat.checkSignature(req,res);
-	wechat.handler(req,res);
-//		res.send('work!!');
-	var api = new wechatAPI(appid, appsecret);	
-	wechat.text(function(data){
-		var msg = {
-      		FromUserName : data.ToUserName,
-      		ToUserName : data.FromUserName,
-      		//MsgType : "text",
-      		Content : "click 'Go to Demo' to see the Demo",
-      		//FuncFlag : 0
-		}		
-    //回复信息
-   		 wechat.send(msg);
-	});
-		
+
+
+router.get('/test', function(req, res){
+	console.log('found it!');
+	var templateId = '9lZRf9WlDz9mgcChIna8fZDBfbSBZPKVlRAy0jY0Meo';
+	var url = 'http://thirdtry.cloudapp.net/myorders';
+	var data = {
+	   "first": {
+	     "value":"Dear tailer, you have a new order placed",
+	     "color":"#173177"
+	   },
+	   "OrderSn":{
+	     "value":"111111",
+	     "color":"#173177"
+	   },
+	   "OrderStatus": {
+	     "value":"The buyer has placed an order",
+	     "color":"#173177"
+	   },
+	   "remark":{
+	     "value":"let's try",
+	     "color":"#173177"
+	   }
+	};
+	//var api = new wechatAPI(appid, appsecret);
+	api.sendTemplate('OPENID', templateId, url, data,   
+		function(err, result){
+			if(err) res.send(err);
+			else res.send(result);
+		}
+	);	
 });
 
 module.exports = router;
